12d8e725769f1bd9d5ae520488113c39fcf2dad3
wenhuihuang <wenhuihu@buffalo.edu>
2022-12-04 17:34:50 -0500

    login and signup in lobby.js

--- static/lobby.js
+++ static/lobby.js
@@ -1,5 +1,6 @@
 $(document).ready(function() {
     var socket = io.connect("http://localhost:5000");
+    // let user
     socket.on('after connect', function(data) {
         // console.log(socket_id)
         console.log("data: ", data)
@@ -10,7 +11,17 @@ $(document).ready(function() {
 
 
     // After User Login, adding into Live user List
-
+    $("#Login").click(function () {
+        let signin_name = $("#signin_username").val();
+        let signin_password = $("#signin_password").val();
+        // $("#login").hide();
+        socket.emit("login", socket_id.toString() + ":sign in:" + signin_name + ":" + signin_password);
+    })
+    $("#SignUp").click(function () {
+        let signup_name = $("#signup_username").val();
+        let signup_password = $("#signup_password").val();
+        socket.emit("signup", socket_id.toString() + ":sign in:" + signup_name + ":" + signup_password);
+    })
 
 
     // Update room situ in the list
